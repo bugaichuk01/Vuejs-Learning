0da7ff2ad568c9f774b39a5a38372fb5f3d7a8ff
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-29 00:37:41 +0300

    Added new features

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,177 @@
 <template>
-    <div class="app">
-        <h1>Страница с постами</h1>
-        <MyButton style="margin: 15px 0;" @click="showDialog">
-            Создать пост
-        </MyButton>
-        <MyDialog v-model:show="dialogVisible">
-            <PostForm @create="createPost"/>
-        </MyDialog>
-        <PostList v-if="!loading" @remove="removePost" :posts="posts"/>
-        <div v-else>Идет загрузка...</div>
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <MyInput placeholder="Поиск..." v-model="searchQuery"/>
+    <div class="app_btns">
+      <MyButton style="margin: 15px 0;" @click="showDialog">
+        Создать пост
+      </MyButton>
+      <MySelect
+          v-model:model-value="selectedSort"
+          :options="sortOptions"
+      />
     </div>
+    <MyDialog v-model:show="dialogVisible">
+      <PostForm @create="createPost"/>
+    </MyDialog>
+    <PostList v-if="!loading" @remove="removePost" :posts="sortedAndSearchedPosts"/>
+    <div v-else>Идет загрузка...</div>
+    <div ref="observer" class="observer"></div>
+<!--    <div class="page__wrapper">
+      <div
+          v-for="pageNumber in totalPages"
+          :key="pageNumber"
+          class="page"
+          :class="{'current_page': page === pageNumber }"
+          @click="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </div>
+    </div>-->
+  </div>
 </template>
 
 <script>
-    import PostForm from "@/components/PostForm";
-    import PostList from "@/components/PostList";
-    import MyDialog from "@/components/UI/MyDialog";
-    import MyButton from "@/components/UI/MyButton";
-    import axios from 'axios';
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import MyDialog from "@/components/UI/MyDialog";
+import MyButton from "@/components/UI/MyButton";
+import axios from 'axios';
+import MySelect from "@/components/UI/MySelect";
+import MyInput from "@/components/UI/MyInput";
 
-    export default {
-        name: 'App',
-        components: {MyButton, MyDialog, PostForm, PostList},
-        data() {
-            return {
-                posts: [],
-                dialogVisible: false,
-                loading: false
-            }
-        },
-        methods: {
-            createPost(post) {
-                this.posts.push(post);
-                this.dialogVisible = false;
-            },
-            removePost(post) {
-                this.posts = this.posts.filter(item => item.id !== post.id);
-            },
-            showDialog() {
-                this.dialogVisible = true;
-            },
-            async fetchPosts() {
-                try {
-                    this.loading = true;
-                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
-                    this.posts = response.data;
-                } catch (e) {
-                    alert('Error')
-                } finally {
-                    this.loading = false;
-                }
-            }
-        },
-        mounted() {
-            this.fetchPosts();
-        }
+export default {
+  name: 'App',
+  components: {MyInput, MySelect, MyButton, MyDialog, PostForm, PostList},
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false,
+      loading: false,
+      selectedSort: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
+      sortOptions: [
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По содержимому'},
+      ],
+      searchQuery: ''
     }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(item => item.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async loadMorePosts() {
+      try {
+        this.page += 1;
+        this.isPostLoading = true;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+        this.posts = [...this.posts, ...response.data];
+      } catch (e) {
+        alert('Error')
+      } finally {
+        this.loading = false;
+      }
+    },
+    async fetchPosts() {
+      try {
+        this.loading = true;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+        this.posts = response.data;
+      } catch (e) {
+        alert('Error')
+      } finally {
+        this.loading = false;
+      }
+    },
+/*    changePage(pageNumber) {
+      this.page = pageNumber;
+    }*/
+  },
+  mounted() {
+    this.fetchPosts();
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+
+    const callback = (entries) => {
+      if (entries[0].isIntersecting) {
+        this.loadMorePosts()
+      }
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer);
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => {
+        return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      })
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+    }
+  },
+  watch: {
+/*    page() {
+      this.fetchPosts();
+    }*/
+  }
+}
 </script>
 
 <style>
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-    .app {
-        padding: 20px;
-    }
+.app {
+  padding: 20px;
+}
+
+.app_btns {
+  display: flex;
+  margin: 15px 0;
+  justify-content: space-between;
+}
+
+/*.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+
+.page {
+  border: 1px solid black;
+  padding: 10px;
+  margin: 5px;
+}
+
+.current_page {
+  border: 2px solid teal;
+}*/
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,27 +1,47 @@
 <template>
-    <div v-if="posts.length > 0">
-        <h3>Список постов</h3>
-        <PostItem @remove="$emit('remove', post)" :post="post" v-for="post in posts" :key="post.id"/>
-    </div>
-    <h2 v-else style="color: red">
-        Список постов пуст
-    </h2>
+  <div v-if="posts.length > 0">
+    <h3>Список постов</h3>
+    <transition-group name="list">
+      <PostItem @remove="$emit('remove', post)" :post="post" v-for="post in posts" :key="post.id"/>
+    </transition-group>
+  </div>
+  <h2 v-else style="color: red">
+    Список постов пуст
+  </h2>
 </template>
 
 <script>
-    import PostItem from "@/components/PostItem";
+import PostItem from "@/components/PostItem";
 
-    export default {
-        components: {PostItem},
-        props: {
-            posts: {
-                type: Array,
-                required: true
-            }
-        }
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
     }
+  }
+}
 </script>
 
 <style scoped>
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
 
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.4s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+.list-move {
+  transition: transform 0.4s ease;
+}
 </style>
